fbcb4b6f6d46e704f7119dde697ff496d1f15504
wheatfox <enkerewpo@hotmail.com>
2024-08-26 08:35:32 +0800

    update

---
+++ board/loongson/ls3a5000/rootfs_ramdisk_overlay/linux2.sh
@@ -0,0 +1,8 @@
+# check whether /dev/hvisor exists, if not, run /install.sh
+if [ ! -e /dev/hvisor ]; then
+    echo "hvisor not installed, installing..."
+    /install.sh
+fi
+
+echo "Starting linux2..."
+hvisor zone start /tool/linux2.json
