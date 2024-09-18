f18d61f0e9853b7a5a1acc512f0e553952ad3f67
wheatfox <enkerewpo@hotmail.com>
2024-09-18 20:03:10 +0800

    update

---
+++ board/loongson/ls3a5000/rootfs_ramdisk_overlay/linux2_virtio.sh
@@ -0,0 +1,9 @@
+# check whether /dev/hvisor exists, if not, run /install.sh
+if [ ! -e /dev/hvisor ]; then
+    echo "hvisor not installed, installing..."
+    /install.sh
+fi
+
+echo "Starting linux2 with virtio..."
+hvisor zone start /tool/linux2.json
+sudo poweroff
