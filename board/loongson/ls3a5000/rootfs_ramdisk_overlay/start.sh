a5acc8415f67e8195cbaa935ab835d8c80396219
wheatfox <enkerewpo@hotmail.com>
2025-05-28 20:39:26 +0800

    Remove hvisor-manager.sh script; update start.sh usage message for screen option; add inittab_nonroot file for system initialization; enhance profile with CPU info and boot instructions for nonroot zones; modify entry points in linux JSON files for consistency; update loongson3a5000_hvisor_defconfig for package configurations.

--- board/loongson/ls3a5000/rootfs_ramdisk_overlay/start.sh
+++ board/loongson/ls3a5000/rootfs_ramdisk_overlay/start.sh
@@ -5,7 +5,7 @@ LINUX_ID=2 # Default to linux2
 
 show_usage() {
     echo "Usage: $0 [-s] [id]"
-    echo "  -s        - Use screen for console"
+    echo "  -s        - Open /dev/pts/0 for after boot. CAUTION: linux1:pts/0,linux2:pts/1,linux3:pts/2"
     echo "  id        - Linux instance to start (1, 2, or 3)"
     echo "              Default: 1"
     exit 1
@@ -49,5 +49,5 @@ hvisor zone start "$CONFIG_FILE"
 
 if [ $USE_SCREEN -eq 1 ]; then
     sleep 1
-    screen /dev/pts/0
+    screen /dev/pts/0 # only for testing linux1
 fi
